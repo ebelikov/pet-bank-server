cdf90073b8ef9f64ea5fa2c477c98c0820233504
Евгений Беликов <e.belikov@x5.ru>
2022-01-23 15:05:34 +0300

    PET-BANK-SERVER: Добавил CORS

--- app/main.js
+++ app/main.js
@@ -2,6 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const newsRoutes = require('../router/news');
+const cors = require('cors');
+
+//TODO: move to separate middleware
+const allowedOrigins = [
+	'http://localhost:3000',
+	'http://localhost:8080',
+];
+
+app.use(cors({
+	origin: function(origin, callback) {
+		if (!origin || allowedOrigins.includes(origin)) {
+			return callback(null, true);
+		}
+
+		const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+
+		return callback(new Error(msg), false);
+	},
+}));
 
 app.use('/api/news', newsRoutes);
 
